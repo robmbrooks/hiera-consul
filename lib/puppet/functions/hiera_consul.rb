dea292e76b2c817e67ca101c8413805cb8b8e9bb
Robert Brooks <robert.brooks@riskiq.net>
2019-02-08 23:46:33 -0800

    add / to search in code

--- lib/puppet/functions/hiera_consul.rb
+++ lib/puppet/functions/hiera_consul.rb
@@ -13,7 +13,7 @@ Puppet::Functions.create_function(:hiera_consul) do
   end
 
   def consul_data_hash(options, context)
-    options['search'] =  ['/'] unless options.key?('search')
+    options['search'] =  [''] unless options.key?('search')
     Diplomat.configure do |config|
       # Set up a custom Consul URL
       config.url = options['url'] if options.key?('url')
@@ -31,7 +31,7 @@ Puppet::Functions.create_function(:hiera_consul) do
 
   def diplomat_kv_get(search)
     begin
-      kv = Diplomat::Kv.get(search, recurse: true, convert_to_hash: true)
+      kv = Diplomat::Kv.get(search + '/', recurse: true, convert_to_hash: true)
       search_path = search.split('/')
       kv = kv.dig(*search_path) unless search_path.length.zero?
       return kv
