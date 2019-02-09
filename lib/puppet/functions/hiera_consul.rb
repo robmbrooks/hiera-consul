73fb4a994b3ea52c9c73a9ab582e110b7ceab42e
Robert Brooks <robert.brooks@riskiq.net>
2019-02-08 23:21:47 -0800

    support multiple search paths

--- lib/puppet/functions/hiera_consul.rb
+++ lib/puppet/functions/hiera_consul.rb
@@ -1,8 +1,10 @@
 Puppet::Functions.create_function(:hiera_consul) do
   begin
+    require 'deep_merge'
     require 'diplomat'
+    require 'backports' unless {}.respond_to? :dig
   rescue LoadError
-    raise Puppet::DataBinding::LookupError, "Error loading diplomat gem library."
+    raise Puppet::DataBinding::LookupError, "Error loading required."
   end
 
   dispatch :consul_data_hash do
@@ -11,6 +13,7 @@ Puppet::Functions.create_function(:hiera_consul) do
   end
 
   def consul_data_hash(options, context)
+    options['search'] =  ['/' ] unless options.key?('search')
     Diplomat.configure do |config|
       # Set up a custom Consul URL
       config.url = options['url'] if options.key?('url')
@@ -19,11 +22,21 @@ Puppet::Functions.create_function(:hiera_consul) do
       # Set extra Faraday configuration options and custom access token (ACL)
       config.options = options['options'] if options.key?('options')
     end
-    begin
-      kv = Diplomat::Kv.get('/', recurse: true, convert_to_hash: true)
-    rescue
-      context.not_found
+    kv = {}
+    options['search'].each do |search|
+      kv = kv.deep_merge(diplomat_kv_get(search))
     end
     return kv
+  end
+
+  def diplomat_kv_get(search)
+    begin
+      kv = Diplomat::Kv.get(search, recurse: true, convert_to_hash: true)
+      search_path = search.split('/')
+      kv = kv.dig(*search_path) unless search_path.length.zero?
+      return kv
+    rescue
+      return {}
+    end
   end
 end
