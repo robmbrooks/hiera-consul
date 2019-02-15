23cb3dde33ae98057da239e76b10e0d23e9162b4
Robert Brooks <robert.brooks@riskiq.net>
2019-02-14 19:30:46 -0800

    remove test upcase function

--- lib/puppet/functions/hiera_consul/upcase.rb
+++
@@ -1,9 +0,0 @@
-Puppet::Functions.create_function(:'hiera_consul::upcase') do
-  dispatch :up do
-    param 'String', :some_string
-  end
-
-  def up(some_string)
-    some_string.upcase
-  end
-end
